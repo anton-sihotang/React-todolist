81403851802fbaad5e41a979361479a011e09903
an7on <67725856+antonsihotan9@users.noreply.github.com>
2020-11-09 17:07:43 +0700

    React Hooks in Todoform

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoForm = () => {
+  const [value, setValue] = useState("");
+
+  const handleFormsubmit = (e) => {
+    e.preventDefault();
+
+    if (!value) {
+      alert("No blank todo!");
+      return;
+    }
+
+    alert(value);
+    setValue("");
+  };
+
   return (
     <section className="add">
-      <form className="add-form">
-        <input type="text" className="add-input" />
+      <form className="add-form" onSubmint={handleFormsubmit}>
+        <input
+          type="text"
+          className="add-input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
         <button className="add-btn main-black-color">Add</button>
       </form>
     </section>
