e957428d35c3bafcaf9b37e2c4e8ba3a63596bb7
an7on <67725856+antonsihotan9@users.noreply.github.com>
2020-11-16 16:32:41 +0700

    Completed todo function

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -7,24 +7,32 @@ import Todos from "../components/Todos";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([
-    { text: "belajar React!" },
-    { text: "belajar Props di React!" },
-    { text: "belajar React Hooks!" }
+    { text: "belajar React!", isCompleted: false },
+    { text: "belajar Props di React!", isCompleted: false },
+    { text: "belajar React Hooks!", isCompleted: false }
   ]);
   const [showAdd, setShowAdd] = useState(false);
 
   const addTodo = (value) => {
-    const addedTodo = [...todos, { text: value }];
+    const addedTodo = [...todos, { text: value, isCompleted: false }];
+
+    setTodos(addedTodo);
+  };
+  const completeTodo = (index) => {
+    const addedTodo = [...todos];
+    addedTodo[index].isCompleted = !addedTodo[index].isCompleted;
 
     setTodos(addedTodo);
   };
   const showAddToggle = () => setShowAdd(!showAdd);
 
+  console.log("todos", todos);
+
   return (
     <Paper>
       <Header showAddToggle={showAddToggle} showAdd={showAdd} />
       <TodoForm addTodo={addTodo} showAdd={showAdd} />
-      <Todos todos={todos} />
+      <Todos todos={todos} completeTodo={completeTodo} />
     </Paper>
   );
 };
