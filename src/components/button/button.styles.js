e9f44ee04b9571952001da6b7a38e8f8aa6002c1
an7on <67725856+antonsihotan9@users.noreply.github.com>
2020-11-30 16:38:36 +0700

    membuat Module untuk semua part component

---
+++ src/components/button/button.styles.js
@@ -0,0 +1,12 @@
+import { css } from "@emotion/core";
+
+export const button = ({ align }) => css`
+  font-size: 1.8rem;
+  font-family: "bungee", sans-serif;
+  width: 24%;
+  padding: 16px;
+  cursor: pointer;
+  background: unset;
+  border: unset;
+  text-align: ${align};
+`;
