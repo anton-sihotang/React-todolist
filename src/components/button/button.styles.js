9abe839d9b76005ae70a31354851e6d35e58a95e
an7on <67725856+antonsihotan9@users.noreply.github.com>
2020-12-04 15:56:15 +0700

    emotioncss props di Button component

--- src/components/button/button.styles.js
+++ src/components/button/button.styles.js
@@ -1,12 +1,29 @@
-import { css } from "@emotion/core";
+import { css } from "@emotion/react";
 
-export const button = ({ align }) => css`
-  font-size: 1.8rem;
-  font-family: "bungee", sans-serif;
-  width: 24%;
-  padding: 16px;
-  cursor: pointer;
-  background: unset;
-  border: unset;
-  text-align: ${align};
-`;
+export const button = ({ color, align }) => {
+  let textColor;
+
+  switch (color) {
+    case "black":
+      textColor = "#4848448";
+      break;
+    case "red":
+      textColor = "#e06262";
+      break;
+    default:
+      textColor = "#484848";
+  }
+
+  return css`
+    font-size: 1.8rem;
+    font-family: "bungee", sans-serif;
+    color: ${textColor};
+    text-align: ${align};
+
+    width: 24%;
+    padding: 16px;
+    cursor: pointer;
+    background: unset;
+    border: unset;
+  `;
+};
