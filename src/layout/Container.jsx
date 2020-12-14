c0f026f7f8daf696c9a1a8a0c3b30eb7c3b6ddf8
an7on <67725856+antonsihotan9@users.noreply.github.com>
2020-12-14 16:59:45 +0700

    implement Layout di Header

---
+++ src/layout/Container.jsx
@@ -0,0 +1,95 @@
+/**@jsx jsx */
+import { jsx, css } from "@emotion/react";
+import PropTypes from "prop-types";
+
+const Container = ({
+  children,
+  flexdirection,
+  flexwrap,
+  justifyContent,
+  alignItems,
+  alignContent
+}) => {
+  const containerStyles = css`
+    display: flex;
+    flex-direction: ${flexdirection};
+    flex-wrap: ${flexwrap};
+    justify-content: ${justifyContent};
+    align-items: ${alignItems};
+    align-content: ${alignContent};
+  `;
+
+  return (
+    <div className="flex-container" css={containerStyles}>
+      {children}
+    </div>
+  );
+};
+
+Container.defaultProps = {
+  flexdirection: "row",
+  flexwrap: "nowrap",
+  justifyContent: "flex-start",
+  alignItems: "stretch",
+  alignContent: "flex-start"
+};
+
+Container.propTypes = {
+  children: PropTypes.oneOf([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]),
+
+  flexdirection: PropTypes.oneOf([
+    "row",
+    "row-reverse",
+    "column",
+    "column-reverse"
+  ]),
+
+  flexwrap: PropTypes.oneOf(["nowrap", "wrap", "wrap-reverse"]),
+
+  justifyContent: PropTypes.oneOf([
+    "flex-start",
+    "flex-end",
+    "center",
+    "space-between",
+    "space-arround",
+    "space-evenly",
+    "start",
+    "end",
+    "left",
+    "right"
+  ]),
+
+  alignItems: PropTypes.oneOf([
+    "stretch",
+    "flex-start",
+    "flex-end",
+    "center",
+    "baseline",
+    "first baseline",
+    "last baseline",
+    "start",
+    "end",
+    "self-start",
+    "self-end"
+  ]),
+
+  alignContent: PropTypes.oneOf([
+    "flex-start",
+    "flex-end",
+    "center",
+    "space-between",
+    "space-around",
+    "space-evenly",
+    "stretch",
+    "start",
+    "end",
+    "baseline",
+    "first baseline",
+    "last baseline"
+  ])
+};
+
+export default Container;
